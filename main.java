9558eca386daff29532ef5337fb7c963732b44c1
titgithub <117897603+titgithub@users.noreply.github.com>
2023-07-11 20:02:17 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+/**
+ *
+ * @author mazum
+ */
+public class main {
+    public static void main(String[]args){
+        NewClass frame = new NewClass();
+    }
+    
+}
